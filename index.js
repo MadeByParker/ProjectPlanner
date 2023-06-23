ab483650574020823fdc8c312c1bad00c6b18ffe
Parker06 <harry.parker@students.plymouth.ac.uk>
2023-06-23 21:48:02 +0100

    Initial deployment

---
+++ index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow, screen } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({
+    width: width,
+    height: height,
+    backgroundColor: '#373848',
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'renderer.js'),
+    },
+    frame: false,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
